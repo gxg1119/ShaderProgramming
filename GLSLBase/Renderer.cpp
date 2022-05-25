f6aaff07a98c751c505ab1254b5f28bc57b75db3
gxg1119 <70965350+gxg1119@users.noreply.github.com>
2022-05-26 06:58:34 +0900

    실습 3
    color, position
    
    per vertex Or per shader

--- GLSLBase/Renderer.cpp
+++ GLSLBase/Renderer.cpp
@@ -74,11 +74,23 @@ void Renderer::CreateVertexBufferObjects()
 		=
 	{
 		0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0,
-	};// 9 floats array
+	}; // 9 floats array
 
 	glGenBuffers(1, &m_VBOLecture2);
 	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture2);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(lecture2), lecture2, GL_STATIC_DRAW);
+
+	float lecture3[]
+		=
+	{
+		0.0, 0.0, 0.0, 1, 0, 0, 1,
+		1.0, 0.0, 0.0, 0, 1, 0, 1,
+		1.0, 1.0, 0.0, 0, 0, 1, 1
+	}; // 21 floats array
+
+	glGenBuffers(1, &m_VBOLecture3);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(lecture3), lecture3, GL_STATIC_DRAW);
 }
 
 void Renderer::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
@@ -344,11 +356,19 @@ void Renderer::Lecture3()
 
 	int attribPosition = glGetAttribLocation(shader, "a_Position");
 	glEnableVertexAttribArray(attribPosition);
-	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture2);
-	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glVertexAttribPointer(attribPosition, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 7, 0);
+
+	int attribColor = glGetAttribLocation(shader, "a_Color");
+	glEnableVertexAttribArray(attribColor);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBOLecture3);
+	glVertexAttribPointer(attribColor, 4, GL_FLOAT, GL_FALSE, sizeof(float) * 7, (GLvoid*)(sizeof(float) * 3));
 
 	int uniformLocTime = glGetUniformLocation(shader, "u_Time");
 	glUniform1f(uniformLocTime, gTime);
+
+	int uniformLocColor = glGetUniformLocation(shader, "u_Color"); // per shader
+	glUniform4f(uniformLocColor, 1, 1, 1, 1);
 
 	glDrawArrays(GL_TRIANGLES, 0, 3);
 
